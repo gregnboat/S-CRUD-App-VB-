ed30d3847b0ffcd10c11c205f668a71db1382b73
Greig Paais <greigpaais@gmail.com>
2021-11-28 14:06:15 +0700

    Adding export to PDF features

--- S CRUD App/Form1.Designer.vb
+++ S CRUD App/Form1.Designer.vb
@@ -41,6 +41,14 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -60,7 +68,7 @@ Partial Class Form1
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label2.Location = New System.Drawing.Point(152, 306)
+        Me.Label2.Location = New System.Drawing.Point(152, 114)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(113, 28)
         Me.Label2.TabIndex = 1
@@ -71,7 +79,7 @@ Partial Class Form1
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label3.Location = New System.Drawing.Point(152, 574)
+        Me.Label3.Location = New System.Drawing.Point(152, 382)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(100, 28)
         Me.Label3.TabIndex = 2
@@ -82,7 +90,7 @@ Partial Class Form1
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label4.Location = New System.Drawing.Point(152, 522)
+        Me.Label4.Location = New System.Drawing.Point(152, 330)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(118, 28)
         Me.Label4.TabIndex = 3
@@ -93,7 +101,7 @@ Partial Class Form1
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label5.Location = New System.Drawing.Point(152, 466)
+        Me.Label5.Location = New System.Drawing.Point(152, 274)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(62, 28)
         Me.Label5.TabIndex = 4
@@ -104,7 +112,7 @@ Partial Class Form1
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label6.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label6.Location = New System.Drawing.Point(152, 415)
+        Me.Label6.Location = New System.Drawing.Point(152, 223)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(77, 28)
         Me.Label6.TabIndex = 5
@@ -115,7 +123,7 @@ Partial Class Form1
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Label7.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label7.Location = New System.Drawing.Point(152, 362)
+        Me.Label7.Location = New System.Drawing.Point(152, 170)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(117, 28)
         Me.Label7.TabIndex = 6
@@ -124,7 +132,7 @@ Partial Class Form1
         'Textproductid
         '
         Me.Textproductid.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Textproductid.Location = New System.Drawing.Point(391, 304)
+        Me.Textproductid.Location = New System.Drawing.Point(391, 112)
         Me.Textproductid.Name = "Textproductid"
         Me.Textproductid.Size = New System.Drawing.Size(352, 30)
         Me.Textproductid.TabIndex = 7
@@ -132,7 +140,7 @@ Partial Class Form1
         'Textitemname
         '
         Me.Textitemname.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Textitemname.Location = New System.Drawing.Point(391, 360)
+        Me.Textitemname.Location = New System.Drawing.Point(391, 168)
         Me.Textitemname.Name = "Textitemname"
         Me.Textitemname.Size = New System.Drawing.Size(352, 30)
         Me.Textitemname.TabIndex = 8
@@ -140,7 +148,7 @@ Partial Class Form1
         'Textdesign
         '
         Me.Textdesign.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Textdesign.Location = New System.Drawing.Point(391, 413)
+        Me.Textdesign.Location = New System.Drawing.Point(391, 221)
         Me.Textdesign.Name = "Textdesign"
         Me.Textdesign.Size = New System.Drawing.Size(352, 30)
         Me.Textdesign.TabIndex = 9
@@ -150,7 +158,7 @@ Partial Class Form1
         Me.Combocolor.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Combocolor.FormattingEnabled = True
         Me.Combocolor.Items.AddRange(New Object() {"Black", "White", "Silver", "Gold"})
-        Me.Combocolor.Location = New System.Drawing.Point(391, 467)
+        Me.Combocolor.Location = New System.Drawing.Point(391, 275)
         Me.Combocolor.Name = "Combocolor"
         Me.Combocolor.Size = New System.Drawing.Size(352, 31)
         Me.Combocolor.TabIndex = 13
@@ -159,7 +167,7 @@ Partial Class Form1
         '
         Me.DateTimePickerinsert.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.DateTimePickerinsert.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePickerinsert.Location = New System.Drawing.Point(391, 524)
+        Me.DateTimePickerinsert.Location = New System.Drawing.Point(391, 332)
         Me.DateTimePickerinsert.Name = "DateTimePickerinsert"
         Me.DateTimePickerinsert.Size = New System.Drawing.Size(352, 30)
         Me.DateTimePickerinsert.TabIndex = 14
@@ -169,7 +177,7 @@ Partial Class Form1
         Me.Radioallowed.AutoSize = True
         Me.Radioallowed.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Radioallowed.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Radioallowed.Location = New System.Drawing.Point(391, 578)
+        Me.Radioallowed.Location = New System.Drawing.Point(391, 386)
         Me.Radioallowed.Name = "Radioallowed"
         Me.Radioallowed.Size = New System.Drawing.Size(91, 27)
         Me.Radioallowed.TabIndex = 15
@@ -182,7 +190,7 @@ Partial Class Form1
         Me.Radionotallowed.AutoSize = True
         Me.Radionotallowed.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Radionotallowed.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Radionotallowed.Location = New System.Drawing.Point(618, 578)
+        Me.Radionotallowed.Location = New System.Drawing.Point(618, 386)
         Me.Radionotallowed.Name = "Radionotallowed"
         Me.Radionotallowed.Size = New System.Drawing.Size(125, 27)
         Me.Radionotallowed.TabIndex = 16
@@ -195,7 +203,7 @@ Partial Class Form1
         Me.Button1.BackColor = System.Drawing.Color.DodgerBlue
         Me.Button1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Button1.Location = New System.Drawing.Point(391, 611)
+        Me.Button1.Location = New System.Drawing.Point(391, 419)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(115, 53)
         Me.Button1.TabIndex = 17
@@ -205,7 +213,7 @@ Partial Class Form1
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(152, 687)
+        Me.DataGridView1.Location = New System.Drawing.Point(186, 495)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 51
         Me.DataGridView1.RowTemplate.Height = 29
@@ -217,7 +225,7 @@ Partial Class Form1
         Me.Button2.BackColor = System.Drawing.Color.DodgerBlue
         Me.Button2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Button2.Location = New System.Drawing.Point(628, 611)
+        Me.Button2.Location = New System.Drawing.Point(628, 419)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(115, 53)
         Me.Button2.TabIndex = 19
@@ -229,7 +237,7 @@ Partial Class Form1
         Me.Button3.BackColor = System.Drawing.Color.DodgerBlue
         Me.Button3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Button3.Location = New System.Drawing.Point(510, 611)
+        Me.Button3.Location = New System.Drawing.Point(510, 419)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(115, 53)
         Me.Button3.TabIndex = 20
@@ -241,19 +249,99 @@ Partial Class Form1
         Me.Button4.BackColor = System.Drawing.Color.DodgerBlue
         Me.Button4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button4.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Button4.Location = New System.Drawing.Point(749, 300)
+        Me.Button4.Location = New System.Drawing.Point(749, 108)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(115, 41)
         Me.Button4.TabIndex = 21
         Me.Button4.Text = "Search"
         Me.Button4.UseVisualStyleBackColor = False
         '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(391, 754)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(352, 30)
+        Me.TextBox1.TabIndex = 23
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label8.Location = New System.Drawing.Point(152, 756)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(55, 28)
+        Me.Label8.TabIndex = 22
+        Me.Label8.Text = "Title"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox2.Location = New System.Drawing.Point(391, 810)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(234, 30)
+        Me.TextBox2.TabIndex = 25
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label9.Location = New System.Drawing.Point(152, 812)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(143, 28)
+        Me.Label9.TabIndex = 24
+        Me.Label9.Text = "Save Location"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 16.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label10.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label10.Location = New System.Drawing.Point(404, 672)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(272, 37)
+        Me.Label10.TabIndex = 26
+        Me.Label10.Text = "Export Table to PDF"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button5.Location = New System.Drawing.Point(640, 806)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(115, 41)
+        Me.Button5.TabIndex = 27
+        Me.Button5.Text = "browse"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button6.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button6.Location = New System.Drawing.Point(464, 863)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(161, 53)
+        Me.Button6.TabIndex = 28
+        Me.Button6.Text = "Export to PDF"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DodgerBlue
-        Me.ClientSize = New System.Drawing.Size(1087, 873)
+        Me.ClientSize = New System.Drawing.Size(1087, 1012)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
@@ -300,4 +388,12 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
 End Class
--- S CRUD App/Form1.vb
+++ S CRUD App/Form1.vb
@@ -1,4 +1,8 @@
 ﻿Imports System.Data.SqlClient
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.IO
+
 
 Public Class Form1
     Dim con As New SqlConnection("Data Source=LAPTOP-F8APJRBB;Initial Catalog=SDatabase;Integrated Security=True")
@@ -34,6 +38,12 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadDataInGrid()
+
+        SaveFileDialog1.FileName = ""
+        SaveFileDialog1.Filter = "PDF (*.pdf)|*.pdf"
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -75,5 +85,73 @@ Public Class Form1
         Dim dt As New DataTable
         sda.Fill(dt)
         DataGridView1.DataSource = dt
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        SaveFileDialog1.FileName = ""
+        If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+            TextBox2.Text = SaveFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim Paragraph As New Paragraph
+        Dim PdfFile As New Document(PageSize.A4, 40, 40, 40, 20)
+        PdfFile.AddTitle(TextBox1.Text)
+        Dim Write As PdfWriter = PdfWriter.GetInstance(PdfFile, New FileStream(TextBox2.Text, FileMode.Create))
+        PdfFile.Open()
+
+        Dim pTitle As New Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 14, iTextSharp.text.Font.BOLD, BaseColor.BLACK)
+        Dim pTable As New Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 12, iTextSharp.text.Font.NORMAL, BaseColor.BLACK)
+
+        Paragraph = New Paragraph(New Chunk(TextBox1.Text, pTitle))
+        Paragraph.Alignment = Element.ALIGN_CENTER
+        Paragraph.SpacingAfter = 5.0F
+
+        PdfFile.Add(Paragraph)
+
+        Dim PdfTable As New PdfPTable(DataGridView1.Columns.Count)
+
+        PdfTable.TotalWidth = 500.0F
+        PdfTable.LockedWidth = True
+
+        Dim widths(0 To DataGridView1.Columns.Count - 1) As Single
+        For i As Integer = 0 To DataGridView1.Columns.Count - 1
+            widths(i) = 1.0F
+        Next
+
+        PdfTable.SetWidths(widths)
+        PdfTable.HorizontalAlignment = 0
+        PdfTable.SpacingBefore = 5.0F
+
+        Dim pdfcell As PdfPCell = New PdfPCell
+
+        For i As Integer = 0 To DataGridView1.Columns.Count - 1
+
+            pdfcell = New PdfPCell(New Phrase(New Chunk(DataGridView1.Columns(i).HeaderText, pTable)))
+
+            pdfcell.HorizontalAlignment = PdfPCell.ALIGN_LEFT
+
+            PdfTable.AddCell(pdfcell)
+
+
+
+        Next
+
+        For i As Integer = 0 To DataGridView1.Rows.Count - 2
+
+            For j As Integer = 0 To DataGridView1.Columns.Count - 1
+                pdfcell = New PdfPCell(New Phrase(DataGridView1(j, i).Value.ToString(), pTable))
+                PdfTable.HorizontalAlignment = PdfPCell.ALIGN_LEFT
+                PdfTable.AddCell(pdfcell)
+            Next
+        Next
+        PdfFile.Add(PdfTable)
+        PdfFile.Close()
+
+        MessageBox.Show("PDF format success exported !", "Informations", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+
     End Sub
 End Class
